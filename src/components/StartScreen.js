74e1d8e43f1894574645471951d7bc94d1b9a6b8
benpemberton <benedictpemberton@gmail.com>
2022-08-31 16:23:41 +0000

    finishing off scss modules

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -5,23 +5,19 @@ import styles from '../sass/components/StartScreen.module'
 const StartScreen = ({ handleStart, isSignedIn }) => {
   return (
     <div className={styles.root}>
-      <div className="main-content-wrap">
-        <div className="image-container">
-          <img src={Discworld} alt="The Discworld on the back of Great Atuin" />
-        </div>
-        <p>
-          Find and select the four Discworld characters in the fastest possible
-          time.
-        </p>
-        <button
-          className="start-button"
-          type="button"
-          onClick={handleStart}
-          disabled={!isSignedIn}
-        >
-          Start the clock!
-        </button>
+      <div className={styles.imageContainer}>
+        <img src={Discworld} alt="The Discworld on the back of Great Atuin" />
       </div>
+      <p>
+        Find and select the four Discworld characters in the fastest possible
+        time.
+      </p>
+      <button
+        onClick={handleStart}
+        disabled={!isSignedIn}
+      >
+        Start the clock!
+      </button>
     </div>
   );
 };
